69f635468126976c9ecb0a625bb83008969545f1
BOBBADI RAJA VARDHAN <bobbadiraja43@gmail.com>
2026-09-17 12:41:48 +0530

    Implement Calculator class and update PolymorphismDemo
    Added a Calculator class with method overloading for addition. Updated main method to demonstrate both method overloading and overriding.

---
+++ 14_Polymorphism/solution.java
@@ -0,0 +1,41 @@
+class Calculator {
+
+    // Method Overloading
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+class Animal {
+
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+
+    // Method Overriding
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class PolymorphismDemo {
+
+    public static void main(String[] args) {
+
+        Calculator c = new Calculator();
+
+        System.out.println("Sum of two numbers: " + c.add(10, 20));
+        System.out.println("Sum of three numbers: " + c.add(10, 20, 30));
+
+        Animal a = new Dog();
+        a.sound();
+    }
+}
