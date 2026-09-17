62eb71a040f1d2731b28b0130bef26f1db0c0e3e
BOBBADI RAJA VARDHAN <bobbadiraja43@gmail.com>
2026-09-17 12:44:04 +0530

    Implement exception handling for division by zero

---
+++ 16_ExceptionHandling/solution.java
@@ -0,0 +1,20 @@
+public class ExceptionHandlingDemo {
+
+    public static void main(String[] args) {
+
+        try {
+            int a = 10;
+            int b = 0;
+
+            int result = a / b;
+
+            System.out.println("Result: " + result);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Exception: Cannot divide by zero");
+        }
+        finally {
+            System.out.println("Finally block is executed");
+        }
+    }
+}
